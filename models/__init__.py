5f783b48fab40c941c99910ea87affc4cdcd4c02
Steve Farrell <sparticlesteve@gmail.com>
2020-05-01 11:36:18 -0700

    fix build_model call

--- models/__init__.py
+++ models/__init__.py
@@ -13,7 +13,7 @@ def get_model(name, **model_args):
     # First look in our local modules using importlib
     try:
         module = importlib.import_module('.' + name, 'models')
-        return module.get_model(**model_args)
+        return module.build_model(**model_args)
 
     # If the import fails, try getting it from torchvision
     except ImportError:
