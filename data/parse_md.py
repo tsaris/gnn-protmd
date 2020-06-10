ae9ee492d0de10d4c1474fc77f51814f70626b40
Aristeidis Tsaris <aristeidistasris@gmail.com>
2020-06-10 14:34:30 -0400

    new

--- data/parse_md.py
+++ data/parse_md.py
@@ -10,12 +10,6 @@ import os
 import numpy as np
 import random
 
-from  scipy.spatial.distance import euclidean, cosine
-from scipy.stats import percentileofscore as perc
-from scipy.spatial.distance import pdist
-
-from itertools import combinations
-
 import torch
 import torch_geometric
 from torch_geometric.datasets import TUDataset
@@ -26,39 +20,19 @@ import tensorflow as tf
 
 def load_graph(filename):
 
-    protein = np.loadtxt(filename)
-    
-    # Make all the combinations
-    edge_np = combinations(np.arange(protein.shape[0]), 2)
-    edge_np = np.array(list(edge_np))
-    edge_np_f = np.flip(edge_np)
-    edge_np = np.concatenate((edge_np, edge_np_f[::-1]), axis=0)
+    npzfile = np.load(filename)
 
-    # Edge features
-    dist = np.array(pdist(protein[:,-3:].astype('float')))
-    dist2 = np.concatenate((dist, dist), axis=0)
-    dist3 = dist2.reshape(dist2.shape[0], 1)
+    edge_np = npzfile['edgelist']
+    dist3 = npzfile['distlist']
+    nd_labels = npzfile['nodefeat']
 
-    # Remove the edges and edge features with distance > 10 A (find an optimized way of this)
-    list_ = []
-    for i in range(0, len(dist3)):
-        if (dist3[i]>6): list_.append(i)
-    edge_np = np.delete(edge_np, list_, axis=0)
-    dist3 = np.delete(dist3, list_, axis=0)
-
-    # Make the edge tensor
     edge_index = torch.tensor(edge_np, dtype=torch.long)
     edge_attr = torch.tensor(dist3, dtype=torch.float)
-    # Make the node type
-    nd_labels = tf.keras.utils.to_categorical(protein[:,0], num_classes=23)
-    # Add node feature of relative position
-    nd_labels = np.hstack((nd_labels, protein[:,[1]]))
-    # Make the node tensor
     x = torch.tensor(nd_labels, dtype=torch.float)
     
     # Make the labels
-    if (filename.endswith('off_a.txt')): y = torch.tensor([0], dtype=torch.int)
-    if (filename.endswith('on_a.txt')): y = torch.tensor([1], dtype=torch.int)
+    if (filename.endswith('off.npz')): y = torch.tensor([0], dtype=torch.int)
+    if (filename.endswith('on.npz')): y = torch.tensor([1], dtype=torch.int)
 
     return Data(x=x, edge_index=edge_index.t().contiguous(), y=y, edge_attr=edge_attr)
 
@@ -68,7 +42,7 @@ class MDGraphDataset(Dataset):
     def __init__(self, input_dir=None, n_samples=None):
         input_dir = os.path.expandvars(input_dir)
         filenames = sorted([os.path.join(input_dir, f) for f in os.listdir(input_dir)
-                            if f.endswith('.txt')])
+                            if f.endswith('.npz')])
         random.shuffle(filenames)
         random.shuffle(filenames)
         self.filenames = filenames if n_samples is None else filenames[:n_samples]
--- scripts/prepare.py
+++ scripts/prepare.py
@@ -3,31 +3,21 @@ from  scipy.spatial.distance import euclidean
 from scipy.stats import percentileofscore as perc
 from scipy.spatial.distance import pdist
 from itertools import combinations
+import tensorflow as tf
 
 residues = ['ALA', 'ARG', 'ASN', 'ASP', 'ASX', 'CYS', 'GLN',
             'GLU', 'GLX', 'GLY', 'HIS', 'ILE', 'LEU', 'LYS',
             'MET', 'PHE', 'PRO', 'SER', 'THR', 'TRP', 'TYR',
             'UNK', 'VAL']
 
-def parse_pdb(path, chain='A', all_chains=False, first=False):
-    '''
-    '''
+def parse_pdb(path, label):
     # Parse residue, atom type and atomic coordinates
-    seq_data = []
-    helix_data = []
-    beta_data = []
-    complex_data = {}
     protein_data = []
     protein_data_all = []
     residue_depth_percentile = []
     res_ = None
     res_i = None
     res_c = None
-    sidechain_data = []
-    sidechain_flag = False
-    sidechain_counter = 0
-    model=0
-    count=0
 
     # Parse the pdb file
     with open(path, 'r') as f:
@@ -53,44 +43,44 @@ def parse_pdb(path, chain='A', all_chains=False, first=False):
         chain_centroid = np.mean(chain_c,axis=0)
         residue_depth = np.array([euclidean(chain_centroid, c) for c in chain_c])
         residue_depth_percentile.append([1- perc(residue_depth, d)/100.0 for d in residue_depth])
-
+    
     # Add the feature to data
-    protein_data_all = np.asarray(protein_data_all)
-    residue_depth_percentile = np.asarray(residue_depth_percentile, dtype='str')
-    protein_add_feature = np.zeros((len(protein_data_all),
-                                    len(protein_data_all[0]),
-                                    len(protein_data_all[0][0])+5))
-    protein_add_feature = np.asarray(protein_add_feature, dtype='str')
-    tmp2 = ["0.0" for x in range(len(protein_data_all[0][0])+5)]
+    protein_data_all = np.asarray(protein_data_all, dtype=np.float32)
+    residue_depth_percentile = np.asarray(residue_depth_percentile, dtype=np.float32)
 
-    for graph_i in range(len(protein_data_all)):
-        for j in range(len(protein_data_all[graph_i])):
-            tmp = np.append(protein_data_all[graph_i][j], residue_depth_percentile[graph_i][j])
-            tmp2[0] = int(1)
-            tmp2[1:3] = tmp[:2]
-            tmp2[4] = tmp[5]
-            tmp2[7:] = tmp[2:5]
-            protein_add_feature[graph_i][j] = tmp2
+    for x in range(0, protein_data_all.shape[0]):
+        res_depth_perc = residue_depth_percentile[x].reshape(residue_depth_percentile[x].shape[0], 1)
+        protein  = np.hstack((res_depth_perc, protein_data_all[x,:,1:5]))
 
-    # Return an arrays of strings
-    protein_add_feature = np.asarray(protein_add_feature, dtype='str')
-    return protein_add_feature
+        # Make all the combinations
+        edge_np = combinations(np.arange(protein.shape[0]), 2)
+        edge_np = np.array(list(edge_np))
+        edge_np_f = np.flip(edge_np)
+        edge_np = np.concatenate((edge_np, edge_np_f[::-1]), axis=0)
+
+        # Edge features
+        dist = np.array(pdist(protein[:,-3:].astype('float')))
+        dist2 = np.concatenate((dist, dist), axis=0)
+        dist3 = dist2.reshape(dist2.shape[0], 1)
+
+        # Remove the edges and edge features with distance > 10 A (find an optimized way of this)
+        list_ = []
+        for i in range(0, len(dist3)):
+            if (dist3[i]>6): list_.append(i)
+        edge_np = np.delete(edge_np, list_, axis=0)
+        dist3 = np.delete(dist3, list_, axis=0)
+
+        # Make the node type
+        nd_labels = tf.keras.utils.to_categorical(protein[:,1], num_classes=23)
+        # Add node feature of relative position
+        nd_labels = np.hstack((nd_labels, protein[:,[0]]))
+
+        # Save the file
+        file_name = "/gpfs/alpine/world-shared/stf011/atsaris/datagnn/datagnn_ras_2020/graph/%d_ras_%s.npz"%(x, label)
+        np.savez(file_name, edgelist=edge_np, distlist=dist3, nodefeat=nd_labels)
 
 
-#protein_all_ON = parse_pdb("/gpfs/alpine/world-shared/stf011/atsaris/datagnn/datagnn_ras_2020/pdb/small_on.pdb")
-#protein_all_OFF = parse_pdb("/gpfs/alpine/world-shared/stf011/atsaris/datagnn/datagnn_ras_2020/pdb/small_off.pdb")
+parse_pdb("/gpfs/alpine/world-shared/stf011/atsaris/datagnn/datagnn_ras_2020/pdb/small_on.pdb", "on")
+parse_pdb("/gpfs/alpine/world-shared/stf011/atsaris/datagnn/datagnn_ras_2020/pdb/small_off.pdb", "off")
 
-protein = parse_pdb("/gpfs/alpine/world-shared/stf011/atsaris/datagnn/datagnn_ras_2020/pdb/small_on_tmp.pdb")
-
-print(protein)
-
-exit(-1)
-for i in range(0, protein_all_ON.shape[0]):
-    file_name = "/gpfs/alpine/world-shared/stf011/atsaris/datagnn/datagnn_ras_2020/graph/%d_ras_on_a.txt"%(i)
-    tmp = protein_all_ON[i].astype(np.float32)
-    np.savetxt(file_name, tmp[:,[2,4,7,8,9]], delimiter=' ')
-
-for i in range(0, protein_all_OFF.shape[0]):
-    file_name = "/gpfs/alpine/world-shared/stf011/atsaris/datagnn/datagnn_ras_2020/graph/%d_ras_off_a.txt"%(i)
-    tmp = protein_all_OFF[i].astype(np.float32)
-    np.savetxt(file_name, tmp[:,[2,4,7,8,9]], delimiter=' ')
+#parse_pdb("/gpfs/alpine/world-shared/stf011/atsaris/datagnn/datagnn_ras_2020/pdb/small_on_tmp.pdb", "on")
